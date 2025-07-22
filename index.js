934b5e2fcbe4eb86695bf5c3d3ac9f0eba6ef65a
sahil <156651037+sahiljaat2278@users.noreply.github.com>
2025-07-22 15:27:07 +0530

    Lec-9
    LEC-9

--- index.js
+++ index.js
@@ -1,52 +1,83 @@
-const express = require('express');
-const app = express();  
+// //create server using express'
+// const express = require('express');
+// const app = express();
 
 // app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-//   res.send("<h1>Hello</h1>");
-//   res.sendFile(__dirname + '/index.html'); //serves the index.html file
+//   res.send('Hello World!');
 // });
-// res.json({
-//     Name:"Sahil",
-//     age:21
-res.end("Hi")
-// })
+// app.use(express.json());
+// app.post('/submit', (req, res) => {
+//     console.log(req.body);
+//     let name = req.body.name;
+//     let email = req.body.email;
+//     let data = {
+//         name: name,
+//         email: email
+//     };
+//     res.send(data);
+//    console.log(data);
+//    const jsonData = JSON.stringify(data);
+  
+//     fs.appendFile("../User.txt",jsonData,(err)=>{
+//         if(err){
+//             return console.log("error in writing in file ");
+//         }
+//     });
 
-//Path variables is a way to pass dynamic values in the URL
-//how can we pass dynamic values in the URL
-// Example: /user/1234/
-//ways to pass dynamic values in the URL
-// 1.Using query parameters
-app.get('/watch/:id', (req, res) => {
-    const videoId = req.quwery.v; 
-    let nid = req.query.n
-    res.send("Id got it" +" "+videoIdnid+" "+nid);
-
-    
+  
    
+
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000');
+// });
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+const filePath = path.join(__dirname, 'User.txt');
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
 });
 
-// 2. Using params
-app.get('/watch/:v/video/:n', (req, res) => {
-    console. log(req.params.v); 
-    console.log(req.params.n); 
-    res.send(" got it: !!!!");
-    
+app.post('/submit', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+
+    const newUser = {
+        name: name,
+        email: email
+    };
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        let users = [];
+
+        if (!err ) {
+            try {
+                users = JSON.parse(data); // parse existing array
+                if (!Array.isArray(users)) users = []; // fallback
+            } catch {
+                users = [];
+            }
+        }
+
+        users.push(newUser);
+
+        fs.writeFile(filePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            res.send(users); // send updated array
+        });
+    });
 });
 
-
-
-// 3. Using request body
-// Path variables are part of the URL and are defined in the route
-
-
-
-
-
-//Differece between res.send and res.end
-// res.send can send a response with a body, while res.end is used to end the response without sending any additional data.
-
-app.listen(3000, function() {  //starts the server on port 3000
-  console.log('Server is running on http://localhost:3000');
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
 });
-//ports communication end point (physical and virtual)
